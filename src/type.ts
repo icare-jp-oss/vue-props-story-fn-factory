0828caeeb22b22533d0e31895fa5adf0eff9e7b6
kahirokunn <okinakahiro@gmail.com>
2020-10-01 08:48:20 +0900

    snapshotをdemo contextに追加した

--- src/type.ts
+++ src/type.ts
@@ -2,10 +2,15 @@ type BaseStoryFn<Props, T> = (a: Props, p: import('@storybook/addons').StoryCont
 
 export type StoryFn<Props = unknown> = BaseStoryFn<Props, import('vue').ComponentOptions<any, any, any, any, any, any>>
 
+type DemoContext = {
+  find: import('puppeteer').ElementHandle['$']
+  snapshot: () => Promise<void>
+}
+
 export type PropsStoryFn<Props = unknown> = StoryFn & {
   args?: Props
   parameters?: {
-    demo?: (context: { find: import('puppeteer').ElementHandle['$'] }) => void | Promise<void>
+    demo?: (context: DemoContext) => void | Promise<void>
     puppeteerTest?: (page: import('puppeteer').Page) => void | Promise<void>
   }
 }
